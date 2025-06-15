6d30b1e9cf4456d25c46002a378fa9df75751dac
supitcha.kam <supitcha.kamo@gmail.com>
2025-06-15 14:48:50 +0700

    update file and color

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Welcome to my portfolio!"!");
+// แสดงข้อความใน console เพื่อตรวจว่า script ทำงาน
+console.log("Welcome to my portfolio!");
+
+// ย่อ header เมื่อ scroll ลง
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
